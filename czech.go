b7973c6c14b805f81cb1c53702b0df8dfa85b2fc
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-02-18 10:16:55 +0100

    improve performance of iban calculator

--- czech.go
+++ czech.go
@@ -15,35 +15,14 @@
 package iban
 
 import (
-	"strconv"
+	"fmt"
 	"strings"
-
-	log "github.com/sirupsen/logrus"
 )
 
-var checkSumToString = make([]string, 99)
-
-func init() {
-	for i := 0; i < 10; i++ {
-		checkSumToString[i] = "0" + strconv.Itoa(i)
-	}
-
-	for i := 10; i < 98; i++ {
-		checkSumToString[i] = strconv.Itoa(i)
-	}
-
-	checkSumToString[98] = "98"
-}
+var itoa = [99]string{"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","96","97","98"}
 
 // CalculateCzech calculates IBAN for Czech Republic
-func CalculateCzech(number, bankCode string) (result string) {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Errorf("iban.CalculateCzech(%s, %s) recovered in %+v", number, bankCode, r)
-			result = ""
-		}
-	}()
-
+func CalculateCzech(number, bankCode string) (string, error) {
 	// canonise input
 	canonisedNumber := strings.Replace(strings.Replace(number, "-", "", -1), " ", "", -1)
 	// accountNumber of length 16
@@ -57,10 +36,7 @@ func CalculateCzech(number, bankCode string) (result string) {
 	// checksum mod 97
 	checksum := (98 - mod97(paddedBankCode+paddedNumber+countryDigits))
 	if checksum == 99 {
-		return
+		return "", fmt.Errorf("invalid number")
 	}
-
-	result = countryCode + checkSumToString[checksum] + paddedBankCode + paddedNumber
-
-	return
+	return countryCode + itoa[checksum] + paddedBankCode + paddedNumber, nil
 }
--- czech_test.go
+++ czech_test.go
@@ -4,21 +4,21 @@ import "testing"
 
 func BenchmarkCalculateCzech(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		calculateCzech("123457", "0710")
+		CalculateCzech("123457", "0710")
 	}
 }
 
 func BenchmarkCalculateCzechParallel(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			calculateCzech("123457", "0710")
+			CalculateCzech("123457", "0710")
 		}
 	})
 }
 
 func TestCalculateCzech(t *testing.T) {
 	expectIBAN := func(a string, b string, c string) {
-		d, err := calculateCzech(a, b)
+		d, err := CalculateCzech(a, b)
 		if err != nil {
 			t.Errorf("%+v", err)
 		}
